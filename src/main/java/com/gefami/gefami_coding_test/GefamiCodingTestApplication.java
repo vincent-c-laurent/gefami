aac843cca2e3fc33b4cff6a8ad49aae1a6b1bc8b
Vincent C <vcl3636@gmail.com>
2025-02-19 19:02:16 +0700

    feat: initial commit

---
+++ src/main/java/com/gefami/gefami_coding_test/GefamiCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.gefami.gefami_coding_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GefamiCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GefamiCodingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gefami/gefami_coding_test/GefamiCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gefami.gefami_coding_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GefamiCodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
